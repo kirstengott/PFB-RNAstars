e08e1052b60b5cd586724926153d75a1a85d2de1
choodson <christina.hodson@ed.ac.uk>
2018-10-29 03:51:17 +0000

    kallisto script

--- scripts/RNAseqpipelineattempt.py
+++ scripts/RNAseqpipelineattempt.py
@@ -14,6 +14,7 @@ import re
 listdir=sys.argv[1]
 index_gtf='../../share/genome/ensembl/Saccharomyces_cerevisiae.R64-1-1.94.gtf'
 index_star='../../share/genomeDir'
+index_kallisto='kallisto/S.cerevisiae_R64_kallisto_index'
 
 filelist=os.listdir(listdir)
 print(filelist)
@@ -33,11 +34,10 @@ print(count)
 count=0
 for files in filelist:
     prefix = 'output/' +str(files[0:10:])
-    
-    
+    kallisto.kallisto_quant(index_kallisto,prefix+'kallisto',listdir+'/'+files)
     count+=1
 
-
+print(count)
 
 
 
