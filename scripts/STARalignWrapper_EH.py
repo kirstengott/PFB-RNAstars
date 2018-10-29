2a80a3176ca93936ee11bbca054e8fab5e93eb08
ehi <ehi@pfb23.cshl.edu>
2018-10-28 20:01:30 -0400

    quickly added comments about appending bam file prefixes if needed

--- scripts/STARalignWrapper_EH.py
+++ scripts/STARalignWrapper_EH.py
@@ -17,7 +17,10 @@ reads = sys.argv[2]
 
 
 def STARalign(genomeDir, readsFile):
-    output = subprocess.check_output('STAR --runThreadN 15 --genomeDir {} --readFilesIn {} --readFilesCommand gunzip -c --quantMode GeneCounts'.format(genomeDir, readsFile), shell=True)
+#    	prefix = str(readsFile[0:10:])
+#	prefix_output = './path/{}'.format(prefix)
+#	need to add '--outFileNamePrefix {}' to subprocess output check
+	output = subprocess.check_output('STAR --runThreadN 15 --genomeDir {} --readFilesIn {} --readFilesCommand gunzip -c --quantMode GeneCounts'.format(genomeDir, readsFile,), shell=True)
     return output
 
 test = STARalign(genome, reads)
